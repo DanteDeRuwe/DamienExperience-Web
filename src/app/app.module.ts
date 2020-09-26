2a9cc4aae0e86fa5d48c3c91c323d1ff2d79b0d3
Jonas Haenebalcke <jonas.haenebalcke@student.hogent.be>
2020-09-26 18:16:28 +0200

    Add nav, pagenotfound component and user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
