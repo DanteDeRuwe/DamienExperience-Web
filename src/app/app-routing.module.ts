c54efd74dbe70da0a016eaf78bc1535690132f32
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-10-10 15:05:51 +0200

    Add map view to map component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent},
+  //temp map
+  { path: 'map', component:MapComponent},
 
   { path: '', redirectTo: 'about', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent}
+  { path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 50.997361;
+  lng = 4.716358;
   constructor() { }
 
   ngOnInit(): void {
+    this.map = new mapboxgl.Map({
+      accessToken: environment.mapbox.accessToken,
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat]
+    })
+
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
