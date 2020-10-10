e794d30d49e59f9dbaa38478d837e9c0360906be
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-10-10 18:17:46 +0200

    Add markers and popups to the map
    At the moment the data is hardcoded as a proof of concept but it is ready to be changed to a service.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,8 +10,42 @@ import { environment } from 'src/environments/environment';
 export class MapComponent implements OnInit {
   map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
+  
+  //temp hardcoded staringpoint
   lat = 50.997361;
   lng = 4.716358;
+
+  //temporary json for checkpoints
+  waypoints = {
+    features: [
+      {
+        title: "Delhaize Tremelo",
+        description: "Gratis blikje frisdrank en snoepreep bij vertoon van een geldige coupon.",
+        coordinates: {
+          longitude: 4.705204,
+          latitude: 50.990410
+        }
+      },
+      {
+        title: "Bevoorrading 1",
+        description: "Hier krijg je een appeltje voor de dorst!",
+        coordinates: {
+          longitude: 4.708955,
+          latitude: 50.994423
+        }
+      },
+      {
+        title: "Voetbalveld",
+        description: "Wachtpost rode kruis",
+        coordinates: {
+          longitude: 4.702002,
+          latitude: 50.986546
+        }
+      }
+    ]
+  }
+  
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,10 +54,19 @@ export class MapComponent implements OnInit {
       container: 'map',
       style: this.style,
       zoom: 13,
-      center: [this.lng, this.lat]
+      center: [this.lng, this.lat],
     })
 
     this.map.addControl(new mapboxgl.NavigationControl());
+    
+    this.waypoints.features.forEach((waypoint) => {
+      var marker = new mapboxgl.Marker()
+      .setLngLat([waypoint.coordinates.longitude, waypoint.coordinates.latitude])
+      .setPopup( new mapboxgl.Popup({ offset: 25 })
+        .setHTML('<h3>' + waypoint.title + '</h3><p>' + waypoint.description + '</p>'))
+      .addTo(this.map);
+
+    })
   }
 
 }
