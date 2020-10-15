af592e45df10e78d62427de8b80de863cbb67a3e
Lucas Van der Haegen <Lucas.vanderhaegen@student.hogent.be>
2020-10-15 21:52:15 +0200

    hide logout button

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -10,7 +10,8 @@ import { AuthenticationService } from '../user/authentication.service';
   styleUrls: ['./main-nav.component.scss'],
 })
 export class MainNavComponent {
-
+  public loggedInUser$ = this._authenticationService.user$;
+  
   constructor(
     private _authenticationService: AuthenticationService,
     private _router: Router) { }
