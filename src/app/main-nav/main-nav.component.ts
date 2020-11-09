0a1ccbe2b5d48bb2de0fe6fa483cf7fcf9e05fb9
NaudtsRuben <47577203+NaudtsRuben@users.noreply.github.com>
2020-11-09 15:08:42 +0100

    save lang in localstorage

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -17,22 +17,31 @@ export class MainNavComponent {
     private _authenticationService: AuthenticationService,
     private _router: Router,
     public translate: TranslateService) {
-      var langs = ['nl', 'fr']
 
-      
-
-      translate.addLangs(langs);
-      var browserLang = navigator.language.substring(0,2)
-
-      console.log(browserLang)
+    //adds dutch and french as supported languages
+    const langs = ['nl', 'fr']
+    translate.addLangs(langs);
 
 
-      if(langs.includes(browserLang)){
+    //checks if language is saved in localstorage
+    //else checks if user's browser's language is supported
+    //default dutch
+    const localLang: string = localStorage.getItem("lang");
+
+    if (localLang) {
+      translate.setDefaultLang(localLang)
+    } else {
+      const browserLang = navigator.language.substring(0, 2)
+
+      if (langs.includes(browserLang)) {
         translate.setDefaultLang(browserLang);
-      }else{
+      } else {
         translate.setDefaultLang('nl');
       }
-     }
+
+      localStorage.setItem("lang", translate.getDefaultLang())
+    }
+  }
 
   register() {
     this._router.navigate(['register']);
@@ -45,15 +54,18 @@ export class MainNavComponent {
   schrijfIn() {
     if (!this.loggedInUser$.value)
       this._router.navigate(['register']);
-    else{
+    else {
       console.log(this.loggedInUser$.value);
-      
+
       this._router.navigate(['registration']);
     }
   }
 
-  changeLang(lang : string): void{
+
+  //changes current language + saves in localstorage
+  changeLang(lang: string): void {
     this.translate.use(lang);
+    localStorage.setItem("lang", lang)
   }
 
 }
