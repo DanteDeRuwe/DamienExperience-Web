97be34b235cda834dca6d3fd4f49e9fbad18404e
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-11-13 10:23:13 +0100

    make sponsors responsive and provide easier way to add them

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
-  styleUrls: ['./sponsors.component.css']
+  styleUrls: ['./sponsors.component.scss']
 })
 export class SponsorsComponent implements OnInit {
 
+  public sponsors: string[] = ["ASA", "coca-cola", "colruyt", "Decathlon", "r2", "spotify", "streamz", "vrt", "vw"] //todo get from service
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
