628fd5f2585e0381526ca6cc779208b1fcf486cd
Lucas Van der Haegen <Lucas.vanderhaegen@student.hogent.be>
2020-11-16 21:10:49 +0100

    Add about tour info starter

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '../map/model/route.model';
+import { RouteDataService } from '../map/services/route-data.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  tourName :string;
+  userName :string;
+
+  routes : Route[] = [];
+
+  currentRoute : Route;
+
+  constructor(private _rds: RouteDataService) { 
+
+  }
 
   ngOnInit(): void {
+    this._rds.getFutureRoutes$().subscribe((routes: Route[]) => {
+      this.routes = routes;
+    });
   }
+
+  
+  get username() : string {
+    return this.userName;
+  }
+  
+  get tourname() : string {
+    return this.tourName;
+  }
+
+  onClick(route:Route) {
+    this.tourName = route.tourName;
+    this.currentRoute = route;
+    //document.getElementById("aboutTourInfo").innerHTML = "{{\"about_tour_info\" | translate}}";
+  }
+
+
 
 }
