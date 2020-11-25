accfca96559d398d4edf137b761b8a8f8c6a86b6
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-11-25 23:14:50 +0100

    Revert unintended change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   mapbox: {
     accessToken: 'pk.eyJ1Ijoiam9yZHl2YW5rZXJrdm9vcmRlIiwiYSI6ImNrZzNvN3o2bTBicncycHBqYzRtZW50dHkifQ.ceuhpKlsJFUPaxinU4qoag'
   },
-  apiUrl: 'https://damiantourapi.azurewebsites.net/api',
+  apiUrl: '/api',
   chatApi: 'http://localhost:3000/',
   liveChatApi: 'https://damienexperience-chat.herokuapp.com',
 };
