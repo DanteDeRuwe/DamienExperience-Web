b6f422d6cdcede53b00ccc1f80f3aba1e1a1c1d0
Jonas Haenebalcke <jonashaenebalcke@hotmail.be>
2020-12-03 16:05:17 +0100

    Add Role Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ import { RegisterComponent } from './user/register/register.component';
 import { UserpageComponent } from './user/userpage/userpage.component';
 import { AddRouteComponent } from './admin/add-route/add-route.component';
 import { AdminNavComponent } from './admin/admin-nav/admin-nav.component';
+import { RoleGuard } from './user/role.guard';
 
 
 const routes: Routes = [
@@ -27,25 +28,27 @@ const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
   { path: 'home', component: HomeComponent },
   { path: 'regulations', component: RegulationsComponent },
-  { path: 'privacypolicy', component: PrivacyPolicyComponent},
+  { path: 'privacypolicy', component: PrivacyPolicyComponent },
   { path: 'sponsors', component: SponsorsComponent },
-  { path: 'cookiepolicy', component: CookiePolicyComponent},
-  { path: 'admin-nav', component: AdminNavComponent ,children:[
-    { path: 'manageroutes', component: ManageroutesComponent},
-    { path: 'dashboard', component: DashboardComponent},
-    { path: 'add-route', component: AddRouteComponent},
-  ]},
+  { path: 'cookiepolicy', component: CookiePolicyComponent },
+  {
+    path: 'admin-nav', component: AdminNavComponent, canActivate: [RoleGuard], children: [
+      { path: 'manageroutes', component: ManageroutesComponent, canActivate: [RoleGuard] },
+      { path: 'dashboard', component: DashboardComponent, canActivate: [RoleGuard] },
+      { path: 'add-route', component: AddRouteComponent, canActivate: [RoleGuard] },
+    ]
+  },
 
 
   //{ path: 'map', component:MapComponent},
   { path: 'register', component: UserpageComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 
   //
   { path: 'add-route', component: AddRouteComponent },
 
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
