2c5f5dbe546f6944b41cdc9dfcc303ce3b5a91a3
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-03 21:41:12 +0100

    Improve tour selector component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,7 +12,7 @@ import { UserDataService } from '../services/user-data.service';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
   registration: FormGroup;
