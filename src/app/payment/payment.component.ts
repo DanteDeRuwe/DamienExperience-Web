4781676a71acf711293f0afa696a0d18ae8de804
NaudtsRuben <47577203+NaudtsRuben@users.noreply.github.com>
2020-12-07 15:47:38 +0100

    fix working payment form

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -6,7 +6,13 @@ import sha1 from 'sha1';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-  hash = sha1("PSPID=damiaanacties*aW2dr86U++ZaKUORDERID=3s*aW2dr86U++ZaKUAMOUNT=5s*aW2dr86U++ZaKUCURRENCY=EURs*aW2dr86U++ZaKULANGUAGE=en_USs*aW2dr86U++ZaKUEMAIL=ruben.naudts@student.hogent.bes*aW2dr86U++ZaKU")
+  hash = sha1(
+    "AMOUNT=500s*aW2dr86U++ZaKU"+
+    "CURRENCY=EURs*aW2dr86U++ZaKU"+
+    "EMAIL=ruben.naudts@student.hogent.bes*aW2dr86U++ZaKU"+
+    "LANGUAGE=nl_NLs*aW2dr86U++ZaKU"+
+    "ORDERID=3s*aW2dr86U++ZaKU"+
+    "PSPID=damiaanacties*aW2dr86U++ZaKU" )
   //CryptoJS.SHA1("PSPID=damiaanacties*aW2dr86U++ZaKUORDERID=3s*aW2dr86U++ZaKUAMOUNT=5s*aW2dr86U++ZaKUCURRENCY=EURs*aW2dr86U++ZaKULANGUAGE=en_USs*aW2dr86U++ZaKUEMAIL=ruben.naudts@student.hogent.bes*aW2dr86U++ZaKU");
 
   constructor() { }
