38a5fe3df2e8b2b96ba59beb1cd5279dffbc6cc7
NaudtsRuben <47577203+NaudtsRuben@users.noreply.github.com>
2020-12-09 10:04:04 +0100

    update payment page

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,5 +1,5 @@
 /*TODO: 
-  Payment component uitbreiden naar volledige pagina
+x  Payment component uitbreiden naar volledige pagina
 x  Registration aangemaakt -> Navigeren naar payment
 x  Payment data meegeven met navigatie
   Api-calls
@@ -7,15 +7,9 @@ x    -> zoeken naar niet betaalde registratie van ingelogde gebruiker
 x    -> flag betaling op true zetten
     -> Registratie overal updaten
   Payment result uitwerken
-  ShaSign encryptie (dynamisch en secret)
-
-
+x  ShaSign encryptie (dynamisch en secret)
 */
-
-
-
 import { Component, OnInit } from '@angular/core';
-import sha1 from 'sha1';
 import { RouteDataService } from '../services/route-data.service';
 import { Payment } from '../models/payment.model';
 import { ActivatedRoute } from '@angular/router';
@@ -27,30 +21,18 @@ import { ActivatedRoute } from '@angular/router';
 export class PaymentComponent implements OnInit {
   payment : Payment 
   orderdShirt : boolean = false
-  amountInEuro : String = "50"
-  hash : string
+  amountInEuro : String = "50,00"
 
   constructor(private route: ActivatedRoute,private _routeRepo : RouteDataService) { }
 
   ngOnInit(): void {
-    console.log(this.hash)
     this.route.data.subscribe(item => {
       this.payment = item['data']
       console.log(this.payment)
       if(this.payment.amount == "6500"){
-        this.amountInEuro = "65"
+        this.amountInEuro = "65,00"
         this.orderdShirt = true
-      }
-      var key = "s*aW2dr86U++ZaKU"
-      var ihash =
-                "AMOUNT=" + this.payment.amount + key +
-                "CURRENCY=" + this.payment.currency + key +
-                "EMAIL=" + this.payment.email + key +
-                "LANGUAGE=" + this.payment.language + key +
-                "ORDERID=" + this.payment.orderId + key +
-                "PSPID=" + this.payment.pspId + key +
-                "USERID=" + this.payment.userId + key;
-      this.hash =sha1(ihash)
+      }      
     })
   }
 }
