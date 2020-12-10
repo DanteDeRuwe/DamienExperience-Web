ff651ca2dddca2fb2e6e3a4156a021f6bbeb737d
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-10 19:13:31 +0100

    Add current location indicator

--- src/app/tracking-map/tracking-map.component.ts
+++ src/app/tracking-map/tracking-map.component.ts
@@ -36,6 +36,10 @@ export class TrackingMapComponent implements OnInit {
 
     //controls toevoegen aan de map
     this.map.addControl(new mapboxgl.NavigationControl());
+
+    this.map.on('load', () => {
+      this.addLocationIndicatorImage();
+    })
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -71,6 +75,7 @@ export class TrackingMapComponent implements OnInit {
 
       let coords = this.walk.walkedPath.coordinates;
       this.addMapLine("walk", color, coords)
+      this.refreshLocationIndicator(coords[coords.length - 1])
     }
   }
 
@@ -119,5 +124,110 @@ export class TrackingMapComponent implements OnInit {
           .setHTML('<h3>' + waypoint.languagesText.title[localLang] + '</h3><p>' + waypoint.languagesText.description[localLang] + '</p>'))
         .addTo(this.map);
     });
+  }
+
+  refreshLocationIndicator(coords: any) {
+    try {
+      this.removeMapLayerAndSource("location")
+    }
+    catch (e) { }
+    finally {
+      this.map.addSource('location', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'Point',
+            'coordinates': coords
+          }
+        }
+      });
+
+      this.map.addLayer({
+        'id': 'location',
+        'type': 'symbol',
+        'source': 'location',
+        'layout': {
+          'icon-image': 'pulsing-dot'
+        }
+      });
+    }
+
+    this.map.flyTo({
+      center: coords,
+      speed: 0.5
+    });
+  }
+
+  addLocationIndicatorImage() {
+    let size = 100;
+    let pulsingDot = {
+      width: size,
+      height: size,
+      data: new Uint8Array(size * size * 4),
+
+      // get rendering context for the map canvas when layer is added to the map
+      onAdd: function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+      },
+
+      // called once before every frame where the icon will be used
+      render: function () {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+
+        var radius = (size / 2) * 0.3;
+        var outerRadius = (size / 2) * 0.7 * t + radius;
+        var context = this.context;
+
+        // draw outer circle
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+          this.width / 2,
+          this.height / 2,
+          outerRadius,
+          0,
+          Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+        context.fill();
+
+        // draw inner circle
+        context.beginPath();
+        context.arc(
+          this.width / 2,
+          this.height / 2,
+          radius,
+          0,
+          Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        // update this image's data with data from the canvas
+        this.data = context.getImageData(
+          0,
+          0,
+          this.width,
+          this.height
+        ).data;
+
+        // continuously repaint the map, resulting in the smooth animation of the dot
+        if (!!this.map) this.map.triggerRepaint();
+
+        // return `true` to let the map know that the image was updated
+        return true;
+      }
+    };
+
+    this.map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
   }
 }
