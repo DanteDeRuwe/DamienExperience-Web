907cce2c5335fb4bc4f023ba62dcdd03a445351f
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-13 21:28:36 +0100

    Add QoL feature: tracking user with url param
    useful for sharing and bookmarking!

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { Route } from '../models/route.model';
 import { Walk } from '../models/walk.model';
@@ -25,9 +26,19 @@ export class TrackingComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
     private _rds: RouteDataService,
-    private _wds: WalkDataService) { }
+    private _wds: WalkDataService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      let email = params['email'];
+      if (!!email) {
+        this.visible = false;
+        this.initWalk(email)
+      }
+    });
+
     this.searchForm = this.fb.group({
       username: ['', Validators.required]
     });
@@ -37,30 +48,34 @@ export class TrackingComponent implements OnInit {
     const searchresult = this.searchForm.value.username;
     this.roomname = searchresult;
     if (searchresult) {
-      this._wds.getWalk$(searchresult)
-        .pipe(take(1)) //only get initial walk
-        .subscribe((walk: Walk) => {
-          if (walk) {
-            this.setupWalk(walk)
-            this.setWalkToLiveWalk(searchresult) //after initializing, use the live walk
-            this.visible = false;
-          } else {
-            console.log('nowalk')
-          }
-        },
-          (err: HttpErrorResponse) => {
-            console.error(err);
-            if (err.error instanceof Error) {
-              this.errorMessage = `Error while trying to get the walk user`
-              console.error(this.errorMessage)
-            } else {
-              this.errorMessage = `Error ${err.status}`;
-              console.error(this.errorMessage)
-            }
-          });
+      this.router.navigate(["/track"], { queryParams: { email: searchresult } })
     } else {
       console.log('nouser')
     }
+  }
+
+  private initWalk(email: string) {
+    this._wds.getWalk$(email)
+      .pipe(take(1)) //only get initial walk
+      .subscribe((walk: Walk) => {
+        if (walk) {
+          this.setupWalk(walk)
+          this.setWalkToLiveWalk(email) //after initializing, use the live walk
+          this.visible = false;
+        } else {
+          console.log('nowalk')
+        }
+      },
+        (err: HttpErrorResponse) => {
+          console.error(err);
+          if (err.error instanceof Error) {
+            this.errorMessage = `Error while trying to get the walk user`
+            console.error(this.errorMessage)
+          } else {
+            this.errorMessage = `Error ${err.status}`;
+            console.error(this.errorMessage)
+          }
+        });
   }
 
   onToggleChat() {
