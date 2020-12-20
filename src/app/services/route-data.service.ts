a666f64bab034314e9a1979a98e471534e20a481
Simon <simon.bettens@live.be>
2020-12-20 15:34:09 +0100

    add error handeling

--- src/app/services/route-data.service.ts
+++ src/app/services/route-data.service.ts
@@ -180,10 +180,10 @@ export class RouteDataService {
     if (err.error instanceof ErrorEvent) {
       errorMessage = `An error occurred: ${err.error.message}`;
     } else if (err instanceof HttpErrorResponse) {
-      errorMessage = `'${err.status} ${err.statusText}' when accessing '${err.url}'`;
+      errorMessage = `'${err.status} ${err.statusText}': ${err.error}`;
     } else {
       errorMessage = err;
-    }
+    } 
     return throwError(errorMessage);
   }
 }
