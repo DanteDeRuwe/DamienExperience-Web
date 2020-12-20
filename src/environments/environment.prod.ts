ae6ca5e06119a4559ea44dc6edbb2af3fc627351
Jordy Van Kerkvoorde <44192604+JordyVanKerkvoorde@users.noreply.github.com>
2020-12-20 16:37:09 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://damiantourapi.azurewebsites.net/api',
+  apiUrl: 'https://damiantourapi.azurewebsites.net/api',
   mapbox: {
     accessToken: 'pk.eyJ1Ijoiam9yZHl2YW5rZXJrdm9vcmRlIiwiYSI6ImNrZzNvN3o2bTBicncycHBqYzRtZW50dHkifQ.ceuhpKlsJFUPaxinU4qoag'
   },
-  apiUrl: '/api',
+  //apiUrl: '/api',
   url : 'https://damianexperience.netlify.app' ,
   //chatApi: 'http://localhost:3000/',
   liveChatApi: 'https://damienexperience-chat.herokuapp.com',
