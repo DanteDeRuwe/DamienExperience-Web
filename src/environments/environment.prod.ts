45dc9d4767ca4bb6b36e36da8e30f5cbdbdfd6a0
Simon <simon.bettens@live.be>
2020-12-20 16:14:12 +0100

    add production variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
-  accessToken: 'pk.eyJ1Ijoiam9yZHl2YW5rZXJrdm9vcmRlIiwiYSI6ImNrZzNvN3o2bTBicncycHBqYzRtZW50dHkifQ.ceuhpKlsJFUPaxinU4qoag',
-  apiUrl: 'https://damiantourapi.azurewebsites.net/api'
+  //apiUrl: 'https://damiantourapi.azurewebsites.net/api',
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoiam9yZHl2YW5rZXJrdm9vcmRlIiwiYSI6ImNrZzNvN3o2bTBicncycHBqYzRtZW50dHkifQ.ceuhpKlsJFUPaxinU4qoag'
+  },
+  apiUrl: '/api',
+  url : 'https://damianexperience.netlify.app' ,
+  //chatApi: 'http://localhost:3000/',
+  liveChatApi: 'https://damienexperience-chat.herokuapp.com',
+  trackingHubUrl: "https://damiantourapi.azurewebsites.net/tracking",
 };
